c22dd3c04e8461680274a1dba1d993d0fd2337b0
den-sad <den-sad@yandex.ru>
2022-11-23 09:31:13 +0300

    Delete unused imports

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Post, Group
-from .forms import *
+from .forms import PostForm
 
 
 POSTS_PER_PAGE: int = 10
