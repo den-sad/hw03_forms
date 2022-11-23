4626dd7557b8a5878141ec9dcb0dc4839268c38b
den-sad <den-sad@yandex.ru>
2022-11-23 09:24:14 +0300

    1st try to review

---
+++ yatube/posts/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        group = forms.ModelChoiceField(
+            queryset=Group.objects.all(),
+            to_field_name="group")
+        fields = ("text", "group")
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'form-control',
+                                          'cols': '40',
+                                          'rows': '10'}),
+        }
---
+++ yatube/posts/views.py
@@ -0,0 +1,111 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Post, Group
+from .forms import *
+
+
+POSTS_PER_PAGE: int = 10
+
+
+def index(request):
+    title = 'Последние обновления на сайте'
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': title,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+    paginator = Paginator(posts, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    title = 'Профайл пользователя ' + author.get_full_name()
+    posts = author.posts.all()
+    post_count = posts.count()
+    paginator = Paginator(posts, POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': title,
+        'author': author,
+        'post_count': post_count,
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    author = get_object_or_404(User, username=post.author)
+    post_count = author.posts.count()
+    title = 'Пост ' + post.text[:30]
+    context = {
+        'title': title,
+        'post': post,
+        'post_count': post_count,
+    }
+
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            response = form.save(commit=False)
+            response.author = request.user
+            response.save()
+            return redirect('posts:profile', request.user)
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+        'is_edit': False,
+    }
+
+    return render(request, 'posts/create_post.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:post_detail', post_id)
+    else:
+        if request.user != post.author:
+            return redirect('posts:post_detail', post_id)
+        form = PostForm(instance=post)
+
+    context = {
+        'form': form,
+        'is_edit': True,
+        'post_id': post_id
+    }
+
+    return render(request, 'posts/create_post.html', context)
